b581d655cd1e265ff4c8e7b81c64b07757571c63
BogdanMglowski@simple.com.pl <BogdanMglowski@simple.com.pl>
2017-05-30 13:40:08 +0200

    first commit from idea

---
+++ src/main/java/pl/bo/Start.java
@@ -0,0 +1,12 @@
+package pl.bo;
+
+/**
+ * Created by bogdan on 30.05.2017.
+ */
+public class Start {
+
+
+    public static void main(String[] args) {
+        System.out.println("Start started");
+    }
+}
